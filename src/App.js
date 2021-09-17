e35e62ae445045b268545445ad79a9ed192fdd9e
Harrison Montoya <harrimv206@gmail.com>
2021-09-17 16:40:16 -0500

    Avanza componente producto

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import './componentes/NavBar';
 import NavBar from './componentes/NavBar';
-
+import Producto from './componentes/Producto';
 
 function App() {
   return (
     <div className="App">
       
         <NavBar></NavBar>
+        <Producto/>
        
     </div>
   );
--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,79 +1,82 @@
 import React from "react";
-
+import biela from '';
 
 
 export default function Producto(){
+
+return(
 <div>
-       <div class="contenedorItems">
-           <div class="contenedorItems__item">
-               <h2 >Kit cilindro FZ16</h2>
-               <img src="./images/kitcilindro.jpg"></img>
-               <input type="number" placeholder="1" >
-               <h3 >$350.000</h3>
-               <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-           </div>
-           <div class="contenedorItems__item">
-            <h2 >Kit Piston XR 250</h2>
-            <img src="./images/kitpiston.png">
-            <input type="number" placeholder="1" >
-            <h3 >$638.000</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
+    <div class="contenedorItems">
         <div class="contenedorItems__item">
-            <h2 >Kit De Arrastre DR 650</h2>
-            <img src="./images/kitdearrastre.jpg">
-            <input type="number" placeholder="1" >
-            <h3 >$220.000</h3>
+            <h2 >Kit cilindro FZ16</h2>
+            <img src={biela}/>
+            <input></input>
+            <h3 >$350.000</h3>
             <button type="submit" class="contenedorItems__item__boton">Comprar</button>
         </div>
-        <div class="contenedorItems__item">
-            <h2 >Bomba de Freno FZ 16</h2>
-            <img src="./images/kitbombafreno.jpg">
-            <input type="number" placeholder="1" >
-            <h3 >$85.000</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
-        <div class="contenedorItems__item">
-            <h2 >Embrague Automatico ATK Flex</h2>
-            <img src="./images/embragueautoakt125flex.jpg">
-            <input type="number" placeholder="1" >
-            <h3 >$288.500</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
-        <div class="contenedorItems__item">
-            <h2 >Manzana Clutch CBF125</h2>
-            <img src="./images/manzanaclutchcbf125.jpg">
-            <input type="number" placeholder="1" >
-            <h3 >$419.500</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
-        <div class="contenedorItems__item">
-            <h2 >Biela FZ16</h2>
-            <img src="./images/bielafz16.jpg">
-            <input type="number" placeholder="1" >
-            <h3 >$150.000</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
-        <div class="contenedorItems__item">
-            <h2 >Kit Cilindro AKT125</h2>
-            <img src="./images/kitcilindro2.jpg">
-            <input type="number" placeholder="1" >
-            <h3 >$210.000</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
-        <div class="contenedorItems__item">
-            <h2 >Llantas Tornado </h2>
-            <img src="./images/llantasahara.png">
-            <input type="number" placeholder="1" >
-            <h3 >$380.000</h3>
-            <button type="submit" class="contenedorItems__item__boton">Comprar</button>
-        </div>
-           
+        {/* <div class="contenedorItems__item">
+         <h2 >Kit Piston XR 250</h2>
+         <img src="./images/kitpiston.png">
+         <input type="number" placeholder="1" >
+         <h3 >$638.000</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+       </div>
+      <div class="contenedorItems__item">
+         <h2 >Kit De Arrastre DR 650</h2>
+         <img src="./images/kitdearrastre.jpg">
+         <input type="number" placeholder="1" >
+         <h3 >$220.000</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+      </div>
+      <div class="contenedorItems__item">
+         <h2 >Bomba de Freno FZ 16</h2>
+         <img src="./images/kitbombafreno.jpg">
+         <input type="number" placeholder="1" >
+         <h3 >$85.000</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+      </div>
+      <div class="contenedorItems__item">
+         <h2 >Embrague Automatico ATK Flex</h2>
+         <img src="./images/embragueautoakt125flex.jpg">
+         <input type="number" placeholder="1" >
+         <h3 >$288.500</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+     </div>
+     <div class="contenedorItems__item">
+         <h2 >Manzana Clutch CBF125</h2>
+         <img src="./images/manzanaclutchcbf125.jpg">
+         <input type="number" placeholder="1" >
+         <h3 >$419.500</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+     </div>
+     <div class="contenedorItems__item">
+         <h2 >Biela FZ16</h2>
+         <img src="./images/bielafz16.jpg">
+         <input type="number" placeholder="1" >
+         <h3 >$150.000</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+     </div>
+     <div class="contenedorItems__item">
+         <h2 >Kit Cilindro AKT125</h2>
+         <img src="./images/kitcilindro2.jpg">
+         <input type="number" placeholder="1" >
+         <h3 >$210.000</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+     </div>
+     <div class="contenedorItems__item">
+         <h2 >Llantas Tornado </h2>
+         <img src="./images/llantasahara.png">
+         <input type="number" placeholder="1" >
+         <h3 >$380.000</h3>
+         <button type="submit" class="contenedorItems__item__boton">Comprar</button>
+     </div>
         
+      */}
 
 
-       </div> 
+    </div> 
 
-    </div>
+ </div>);
+
     
 }
